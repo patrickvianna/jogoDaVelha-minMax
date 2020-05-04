76ab775fa47d6a95a0511ead2d59b5f7c7cc37f9
Patrick Vianna <patrickviannapblv@gmail.com>
2020-05-03 21:00:12 -0300

    Verificando vencedor

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const player1 = "x";
 const player2 = "o";
 let playerTime = player1;
 let gameOver = false;
+let casosSucesso = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
 AtualizarMostrador();
 OnClickEspaco();
@@ -37,4 +38,27 @@ function Jogada() {
     }
 
     AtualizarMostrador();
+    let jogadas = ObterJogadasTabuleiro()
+    VerificarVencedor(jogadas)
+}
+
+function ObterJogadasTabuleiro() {
+    let espacos = [];
+    for(let espaco of document.getElementsByClassName("espaco")){
+        espacos.push(espaco.getAttribute("jogada"));
+    }
+    return espacos;
+}
+
+function VerificarVencedor(tabuleiro) {
+    let vencedor = "";
+    casosSucesso.forEach(element => {
+        if (tabuleiro[element[0] - 1] != "" &&
+            tabuleiro[element[0] - 1] == tabuleiro[element[1] - 1] &&
+            tabuleiro[element[0] - 1] == tabuleiro[element[2] - 1]) {
+            vencedor = tabuleiro[element[0] - 1];
+            alert(`O jogador ${vencedor} ganhou!`)
+        }
+
+    });
 }
